35433b34d81fa450003ac7e356393f75ae2b91fa
GmailHelene <65661117+GmailHelene@users.noreply.github.com>
2025-06-16 00:50:04 +0200

    Delete database.js

--- database.js
+++
@@ -1,6 +0,0 @@
-const database_url = os.getenv('DATABASE_URL')
-if database_url and 'postgres.railway.internal' in database_url:
-    database_url = database_url.replace(
-        'postgres.railway.internal', 
-        os.getenv('PGHOST')
-    )
